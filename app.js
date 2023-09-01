a266bf1f7fdef56b63cebaf8fb6dc60aab44b1b3
Cynthia-Cyber-Code <cynthiaf181999@gmail.com>
2023-09-01 15:02:08 +0200

    simple authentification and jest package

--- app.js
+++ app.js
@@ -36,6 +36,13 @@ const spotsRouter = require('./routes/spot');
 // Implémente l'app qui est une instance d'express
 const app = express();
 
+// Middleware
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 const SECRET_KEY = 'secretkey23456';
 
 const users = [];
@@ -54,31 +61,40 @@ const verifyJWT = (req, res, next) => {
         }
 };
 
-app.post("/signup", async (req, res) => {
+// Sign-up (Inscription)
+app.post('/signup', async (req, res) => {
     console.log(req.body);
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
     console.log(hashedPassword);
-
     const user = {
         username: req.body.username,
         password: hashedPassword
     };
-    users.push(user); // Ajout au tableau users l'utilisateur en cours
-
-    //TODO: Ajout l'utilisateur dans la base de donnée
-
-    console.log(users);
-    res.status(201).json({message: "Utilisateur créé"})
+    users.push(user); // Ajoute au tableau users l'utilisateur en cours
+    // TODO: Ajouter l'utilisateur dans la base de données
+    
+    res.status(201).json({message: "Utilisateur créé"});
 });
 
-// Middleware
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Sign-in (Connexion)
+app.post('/signin', async (req, res) => {
+    const user = users.find(u => u.username === req.body.username);
+    // TODO: Récupérer l'utilisateur depuis la base de données
 
+    if (!user) return res.status(400).send("Nom d'utilisateur ou mot de passe incorrect");
+    
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send("Nom d'utilisateur ou mot de passe incorrect");
+
+    const payload = {
+        username: user.username,
+      // Vous pouvez ajouter d'autres propriétés ici
+    };
+
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: 60 * 60 * 24 });
+    res.json({message: token});
+});
 
 // Implémentation des routes
 app.use('/api', verifyJWT, indexRouter);
